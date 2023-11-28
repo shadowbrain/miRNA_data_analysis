27a8946fd0d49f16b30f3d626873e8f91eb5e357
Brian Butler <shadowbrain@users.noreply.github.com>
2023-11-28 12:16:19 -0500

    added positive normalized data plots and updated file path

--- miRNA_visualizer.py
+++ miRNA_visualizer.py
@@ -4,17 +4,17 @@ import numpy as np
 
 
 # Load your data
-data = pd.read_csv('./visualize_grouped_miRNA_folded_pvalues.csv')
+data = pd.read_csv('./grouped_miRNA_folded_pvalues.csv')
 
 # Create a volcano plot assuming your data has 'Fold_Change' and 'p_Value' columns
 plt.scatter(data['AF_to_AM_FoldChange_HK'], -np.log10(data['AF_AM_p_value_HK']), c='blue', alpha=0.5)
 plt.scatter(data['AF_to_FC_FoldChange_HK'], -np.log10(data['AF_FC_p_value_HK']), c='red', alpha=0.5)
 plt.scatter(data['AM_to_MC_FoldChange_HK'], -np.log10(data['AM_MC_p_value_HK']), c='orange', alpha=0.5)
 plt.scatter(data['FC_to_MC_FoldChange_HK'], -np.log10(data['FC_MC_p_value_HK']), c='green', alpha=0.5)
-plt.scatter(data['AF_to_AM_FoldChange_NEG'], -np.log10(data['AF_AM_p_value_NEG']), c='blue', alpha=0.5)
-plt.scatter(data['AF_to_FC_FoldChange_NEG'], -np.log10(data['AF_FC_p_value_NEG']), c='red', alpha=0.5)
-plt.scatter(data['AM_to_MC_FoldChange_NEG'], -np.log10(data['AM_MC_p_value_NEG']), c='orange', alpha=0.5)
-plt.scatter(data['FC_to_MC_FoldChange_NEG'], -np.log10(data['FC_MC_p_value_NEG']), c='green', alpha=0.5)
+plt.scatter(data['AF_to_AM_FoldChange_POS'], -np.log10(data['AF_AM_p_value_POS']), c='blue', alpha=0.5)
+plt.scatter(data['AF_to_FC_FoldChange_POS'], -np.log10(data['AF_FC_p_value_POS']), c='red', alpha=0.5)
+plt.scatter(data['AM_to_MC_FoldChange_POS'], -np.log10(data['AM_MC_p_value_POS']), c='orange', alpha=0.5)
+plt.scatter(data['FC_to_MC_FoldChange_POS'], -np.log10(data['FC_MC_p_value_POS']), c='green', alpha=0.5)
 plt.title('Volcano Plot')
 plt.xlabel('Fold Change')
 plt.ylabel('-log10(p-value)')
